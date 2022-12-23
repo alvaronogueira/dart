725b6cedd48e62e44e86821c31c0e4169d407eef
alvaronogueira <alvaronogueira29@gmail.com>
2022-12-23 17:52:30 -0300

    firsts steps

--- bin/main.dart
+++ bin/main.dart
@@ -5,9 +5,15 @@ void main() {
   int idade = 30;
   double altura = 1.69;
   bool nerd = (idade == altura);
-  
+  bool geek = false;
+  String name = 'Álvaro P de A Nogueira';
+  String nickname = 'Alvinho';
+  String frase = '$initial! \n'
+  'Eu sou $name,\n' 'mas meu apelido é $nickname.\n'
+  'Eu tenho $idade anos e tenho $altura de altura.\n'
+  'Seria eu Geek? $geek';
 
-  print(nerd);
 
+  print(frase);
 }
 
