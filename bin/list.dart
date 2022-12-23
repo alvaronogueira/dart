d8606080736b076549b02da6ad24ec670ecea65d
alvaronogueira <alvaronogueira29@gmail.com>
2022-12-23 18:13:20 -0300

    Firsts Steps

--- bin/list.dart
+++ bin/list.dart
@@ -1,6 +1,6 @@
 //Criando listas//
 void main() {
-  List<String> listaNomes = ['Alberlane, Wilderlaide, Wevanny, Livisson, Jota'];
-
-  print(listaNomes);
+  List<String> listaNomes = ['Alberlane', 'Wilderlaide', 'Wevanny', 'Livisson', 'Jota'];
+  
+  print(listaNomes.length);
 }
