7390b775e0a5836b2205e6f134f730ef6a86e221
alvaronogueira <alvaronogueira29@gmail.com>
2022-12-23 22:28:05 -0300

    list dynamic

--- bin/list.dart
+++ bin/list.dart
@@ -1,6 +1,20 @@
 //Criando listas//
 void main() {
+  //Lista Comum//
   List<String> listaNomes = ['Alberlane', 'Wilderlaide', 'Wevanny', 'Livisson', 'Jota'];
   
-  print(listaNomes[2]);
+  //Lista Dinâmica//
+  bool geek = false;
+
+  List<dynamic> alvinho = [30, 'Hello, World!', 'alvinho', 1.69, 'Alvaro Nogueira'];
+
+  String frase = '${alvinho[1]}! \n'
+  'Eu sou  ${alvinho[4]},\n' 
+  'mas meu apelido é ${alvinho[2]}.\n'
+  'Eu tenho  ${alvinho[0]} anos e tenho ${alvinho[3]} de altura.\n'
+  'Seria eu Geek? $geek';
+
+
+
+  print(frase);
 }
