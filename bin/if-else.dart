08e293f4f5d0c9dcfe8568a5cd7b1162be3bf5b3
alvaronogueira <alvaronogueira29@gmail.com>
2022-12-25 20:52:43 -0300

    update if-else.dart

--- bin/if-else.dart
+++ bin/if-else.dart
@@ -5,14 +5,23 @@ void main() {
   String name = 'Álvaro Nogueira';
   String nickname;
   bool deMaior;
+  int energia = 100;
+
   if (age >= 18) {
     deMaior = true;
   } else {
     deMaior = false;
-  }  
+  }
+
   String frase = 'Eu sou ${name}, tenho ${age} anos. Eu tenho mais de 18 anos? ${deMaior}';
 
-  for(int i=0; i<5; i++){}
+  for(int i=1; i<=10; i++){
+    print('Concluí $i voltas');
+  }
 
-  print(frase);
+  while(energia>20){
+    print('Mais uma repetição');
+    energia = energia - 10;
+  }
+
 }
