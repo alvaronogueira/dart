ac92bae2f60715dd6f4d9cb069a0d5c96ae1ca14
alvaronogueira <alvaronogueira29@gmail.com>
2022-12-25 20:04:05 -0300

    if-else.dart

---
+++ bin/if-else.dart
@@ -0,0 +1,17 @@
+void main() {
+  String initial = "Hello World";
+  int age = 30;
+  double high = 1.69;
+  String name = 'Álvaro Nogueira';
+  String nickname;
+  bool deMaior;
+  if (age >= 18) {
+    deMaior = true;
+  } else {
+    deMaior = false;
+  }
+  
+  String frase = 'Eu sou ${name}, tenho ${age} anos. Eu tenho mais de 18 anos? ${deMaior}';
+
+  print(frase);
+}
