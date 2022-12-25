d5380f592eec1148338f3f69977351365fa3e95c
alvaronogueira <alvaronogueira29@gmail.com>
2022-12-25 16:39:46 -0300

    list.dart

--- bin/list.dart
+++ bin/list.dart
@@ -1,38 +1,40 @@
 //Criando listas//
 void main() {
- //Lista Comum//
- List<String> listaNomes = ['Alberlane', 'Wilderlaide', 'Wevanny', 'Livisson', 'Jota'];
-  
- //Lista Dinâmica//
- String initial = "Hello World";
- int age = 30;
- double high = 1.69;
- bool geek = false;
- String name = 'Álvaro Nogueira';
- String nickname = 'Alvinho';
+  //Lista Comum//
+  List<String> listaNomes = [
+    'Alberlane',
+    'Wilderlaide',
+    'Wevanny',
+    'Livisson',
+    'Jota'
+  ];
 
+  // Lista Dinâmica // Var, Const e Final //
+  String initial = "Hello World";
+  int age = 30;
+  double high = 1.69;
+  bool geek = false;
+  const String name = 'Álvaro Nogueira';
+  final String nickname;
 
- List<dynamic> alvinho = [30, 'Hello, World!', 'alvinho', 1.69, 'Alvaro Nogueira'];
+  nickname = 'alvinho';
 
- String frase = '${alvinho[1]}! \n'
-  'Eu sou  ${alvinho[4]},\n' 
-  'mas meu apelido é ${alvinho[2]}.\n'
-  'Eu tenho  ${alvinho[0]} anos e tenho ${alvinho[3]} de altura.\n'
-  'Seria eu Geek? $geek';
-  
+  List<dynamic> alvinho = [
+    30,
+    'Hello, World!',
+    'alvinho',
+    1.69,
+    'Alvaro Nogueira'
+  ];
 
+  String frase = '${alvinho[1]}! \n'
+      'Eu sou  ${alvinho[4]},\n'
+      'mas meu apelido é ${alvinho[2]}.\n'
+      'Eu tenho  ${age = 420} anos e tenho ${alvinho[3]} de altura.\n'
+      'Seria eu Geek? $geek';
 
+  print(frase);
 
-
- // Lista dinâmica 2 //
- List<dynamic> aleatorio = [age, initial, nickname, high, name];
-
- print(frase);
-
-
-
-
-
- // Var, Const e Final //
-  
+  // Lista dinâmica 2 //
+  List<dynamic> aleatorio = [age, initial, nickname, high, name];
 }
