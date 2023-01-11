7ffac942efda5e389804ba9c61e22a8c9ee8ee64
alvaronogueira <alvaronogueira29@gmail.com>
2023-01-11 05:55:54 -0300

    up account.dart

---
+++ bin/models/account.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+class Account {
+  String name;
+  double balance;
+  bool isAuthenticated;
+
+  Account(
+      {required this.name,
+      required this.balance,
+      required this.isAuthenticated}
+    );
+
+  editBalance({required value}) {
+    balance = balance + value;
+  }
+}
