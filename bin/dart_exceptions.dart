a6f8c988544afa14c46242b745ac64640dd523e6
alvaronogueira <alvaronogueira29@gmail.com>
2023-01-16 06:51:12 -0300

    up

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -1,11 +1,11 @@
-import 'controllers/bank_controller.dart';
+/*import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
 void main() {
-  // Creating the bank
+ Creating the bank
   BankController bankController = BankController();
 
-  // Add account
+ Add account
   bankController.addAccount(
       id: "Elon",
       account:
@@ -16,10 +16,32 @@ void main() {
       account:
           Account(name: "Diego Rosa", balance: 600, isAuthenticated: true));
 
-  // Making transfer
+ Making transfer
   bool result = bankController.makeTransfer(
       idSender: "Digo", idReceiver: "Elon", amount: 700);
 
-  // Watching result
+ Watching result
   print(result);
+}
+*/
+
+void main() {
+  print("Started Main");
+  functionOne();
+  print("Finished Main");
+}
+
+void functionOne() {
+  print("Started F01");
+  functionTwo();
+  print("Started F02");
+}
+
+void functionTwo() {
+  print("Started F02");
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+    double amount = double.parse("isso é um teste");
+  }
+  print("Finished F02");
 }
